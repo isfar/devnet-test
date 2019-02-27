dab4d292ffb0e5e12014ad184452d1e9ec0c2727
isfar <isfar.sifat@gmail.com>
2019-02-27 21:24:00 +0600

    Added Mo module and Zend\Mvc\Console

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,9 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Mvc\Console',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'Mo',
 ];
